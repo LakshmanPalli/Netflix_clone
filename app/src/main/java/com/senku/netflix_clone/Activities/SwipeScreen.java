8ed50dc5b44a5bf89e704b450f999bd665c447b7
venkata sai lakshman palli <66459612+LakshmanPalli@users.noreply.github.com>
2021-06-28 15:29:45 +0530

    swipe screen part-2

---
+++ app/src/main/java/com/senku/netflix_clone/Activities/SwipeScreen.java
@@ -0,0 +1,16 @@
+package com.senku.netflix_clone.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.senku.netflix_clone.R;
+
+public class SwipeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_screen);
+    }
+}
---
+++ app/src/main/java/com/senku/netflix_clone/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.senku.netflix_clone.Adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.senku.netflix_clone.R;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images={R.drawable.ic_net,R.drawable.netflix_img, R.drawable.ic_netflix_logo_icon_170918};
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageviewforcustomlayout);
+        imageView.setImageResource(images[position]);
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
