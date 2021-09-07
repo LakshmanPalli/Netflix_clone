a91153d084329502a4c1945b50545bbad5ec35c3
LakshmanPalli <luky21433@gmail.com>
2021-09-07 20:40:28 +0530

    Netflix Toolbars for main screens

--- app/src/main/java/com/senku/netflix_clone/MainScreens/MainScreen.java
+++ app/src/main/java/com/senku/netflix_clone/MainScreens/MainScreen.java
@@ -1,16 +1,37 @@
 package com.senku.netflix_clone.MainScreens;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.senku.netflix_clone.R;
 
 public class MainScreen extends AppCompatActivity {
+    TextView movietext, tvseriestext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main_screen);
+        movietext = findViewById(R.id.moviesId);
+        tvseriestext = findViewById(R.id.tvseriesId);
+        movietext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, Movies.class);
+                startActivity(i);
+            }
+        });
+        tvseriestext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainScreen.this, TvSeries.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/senku/netflix_clone/MainScreens/Settings.java
@@ -0,0 +1,17 @@
+package com.senku.netflix_clone.MainScreens;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.senku.netflix_clone.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_settings);
+    }
+}
